c5e2eb5fabd78a0e626ca71a526a092d35b584ef
Falcon-Peregrine <falcon81321@gmail.com>
2019-05-26 17:03:10 +0800

    20190526

--- CentOS7/unrar.sh
+++ CentOS7/unrar.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-for file in *.part1.rar; do unrar x -phttps://www.myhd1080.tv ${file} /data/av; done;
 rm -rf *.aria2;
+for file in *.part1.rar; do unrar x -phttps://www.myhd1080.tv ${file} /data/av; done;
 for file in */ .*/ ; do echo "$file is a directory"; done;
 for dir in */; do dir=${dir%*/}; echo ready: ${dir##*/}; done;
 
